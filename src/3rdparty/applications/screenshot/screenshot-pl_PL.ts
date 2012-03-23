4b9b97e69f03a7b4784f772f797202539d4e05bb
Yann Dirson <ydirson@free.fr>
2012-03-23 22:43:01 +0100

    New translation files, including french translations.

---
+++ src/3rdparty/applications/screenshot/screenshot-pl_PL.ts
@@ -0,0 +1,113 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="pl_PL">
+<context>
+    <name>Screenshot</name>
+    <message>
+        <location filename="screenshot.cpp" line="52"/>
+        <source>Screenshot</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="screenshot.cpp" line="102"/>
+        <source>/untitled.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="screenshot.cpp" line="117"/>
+        <source>Save As</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="screenshot.cpp" line="119"/>
+        <source>%1 Files (*.%2);;All Files (*)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="screenshot.cpp" line="152"/>
+        <source>Upload confirmation</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="screenshot.cpp" line="153"/>
+        <source>Do you want to upload this screenshot to http://%1 ?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="screenshot.cpp" line="154"/>
+        <source>&amp;No</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="screenshot.cpp" line="154"/>
+        <source>&amp;Yes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="screenshot.cpp" line="203"/>
+        <source>Unable to upload screenshot</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="screenshot.cpp" line="207"/>
+        <source>Your screenshot is uploaded successfully</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="screenshot.cpp" line="228"/>
+        <source>Options</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="screenshot.cpp" line="231"/>
+        <source> s</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="screenshot.cpp" line="235"/>
+        <source>Screenshot Delay:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="screenshot.cpp" line="237"/>
+        <source>Hide This Window</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="screenshot.cpp" line="251"/>
+        <source>New</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="screenshot.cpp" line="252"/>
+        <source>Save</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="screenshot.cpp" line="253"/>
+        <source>Upload</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="screenshot.cpp" line="254"/>
+        <location filename="screenshot.cpp" line="265"/>
+        <source>Quit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="screenshot.cpp" line="256"/>
+        <source>New Screenshot</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="screenshot.cpp" line="259"/>
+        <source>Save Screenshot</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="screenshot.cpp" line="262"/>
+        <source>Upload Screenshot</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ src/3rdparty/games/qtmaze/qtmaze-es_ES.ts
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="es_ES">
+<context>
+    <name>FormBase</name>
+    <message>
+        <location filename="formbase.ui" line="14"/>
+        <source>QtMaze</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="formbase.ui" line="107"/>
+        <source>Level number</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="formbase.ui" line="147"/>
+        <source>Touch the screen to continue</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
