227379fe75a1d55aac9fda50d278ad90d7375228
Yann Dirson <ydirson@free.fr>
2012-03-25 16:48:23 +0200

    Normalize handling of libraries/handwriting translations using TRANSLATION_SUBDIR.

---
+++ src/libraries/handwriting/translations/libqmstroke-da_DK.ts
@@ -0,0 +1,79 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="da_DK">
+<context>
+    <name>Handwriting</name>
+    <message>
+        <location filename="../char.cpp" line="40"/>
+        <source>[Esc]</source>
+        <translation>[Esc]</translation>
+    </message>
+    <message>
+        <location filename="../char.cpp" line="41"/>
+        <source>[Tab]</source>
+        <translation>[Tab]</translation>
+    </message>
+    <message>
+        <location filename="../char.cpp" line="42"/>
+        <source>[BackSpace]</source>
+        <translation>[Tilbage knap]</translation>
+    </message>
+    <message>
+        <location filename="../char.cpp" line="43"/>
+        <source>[Return]</source>
+        <translation>[Retur]</translation>
+    </message>
+    <message>
+        <location filename="../char.cpp" line="44"/>
+        <source>[Uppercase]</source>
+        <translation>[Store bogstaver]</translation>
+    </message>
+    <message>
+        <location filename="../char.cpp" line="45"/>
+        <source>[Caps Lock]</source>
+        <translation>[Lås skift]</translation>
+    </message>
+    <message>
+        <location filename="../char.cpp" line="46"/>
+        <source>[Shortcut]</source>
+        <translation>[Genvej]</translation>
+    </message>
+    <message>
+        <location filename="../char.cpp" line="47"/>
+        <source>[Punctuation]</source>
+        <translation>[Tegnsætning]</translation>
+    </message>
+    <message>
+        <location filename="../char.cpp" line="48"/>
+        <source>[Symbol]</source>
+        <translation>[Symbol]</translation>
+    </message>
+    <message>
+        <location filename="../char.cpp" line="49"/>
+        <source>[Next Word]</source>
+        <translation>[Næste ord]</translation>
+    </message>
+    <message>
+        <location filename="../char.cpp" line="50"/>
+        <source>[Word Menu]</source>
+        <translation>[Ord menu]</translation>
+    </message>
+    <message>
+        <location filename="../char.cpp" line="51"/>
+        <source>[Symbol Menu]</source>
+        <translation>[Symbol menu]</translation>
+    </message>
+    <message>
+        <location filename="../char.cpp" line="52"/>
+        <source>[Mode Menu]</source>
+        <translation>[Tilstandsmenu]</translation>
+    </message>
+    <message>
+        <location filename="../char.cpp" line="566"/>
+        <location filename="../char.cpp" line="577"/>
+        <source>Unnamed</source>
+        <comment>Character set name</comment>
+        <translation>Unavngivet</translation>
+    </message>
+</context>
+</TS>
--- src/libraries/handwriting/translations/libqmstroke-pl_PL.ts
+++ src/libraries/handwriting/translations/libqmstroke-pl_PL.ts
@@ -1,76 +1,76 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="es_ES">
+<TS version="2.0" language="pl_PL">
 <context>
     <name>Handwriting</name>
     <message>
-        <location filename="char.cpp" line="40"/>
+        <location filename="../char.cpp" line="40"/>
         <source>[Esc]</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="char.cpp" line="41"/>
+        <location filename="../char.cpp" line="41"/>
         <source>[Tab]</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="char.cpp" line="42"/>
+        <location filename="../char.cpp" line="42"/>
         <source>[BackSpace]</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="char.cpp" line="43"/>
+        <location filename="../char.cpp" line="43"/>
         <source>[Return]</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="char.cpp" line="44"/>
+        <location filename="../char.cpp" line="44"/>
         <source>[Uppercase]</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="char.cpp" line="45"/>
+        <location filename="../char.cpp" line="45"/>
         <source>[Caps Lock]</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="char.cpp" line="46"/>
+        <location filename="../char.cpp" line="46"/>
         <source>[Shortcut]</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="char.cpp" line="47"/>
+        <location filename="../char.cpp" line="47"/>
         <source>[Punctuation]</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="char.cpp" line="48"/>
+        <location filename="../char.cpp" line="48"/>
         <source>[Symbol]</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="char.cpp" line="49"/>
+        <location filename="../char.cpp" line="49"/>
         <source>[Next Word]</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="char.cpp" line="50"/>
+        <location filename="../char.cpp" line="50"/>
         <source>[Word Menu]</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="char.cpp" line="51"/>
+        <location filename="../char.cpp" line="51"/>
         <source>[Symbol Menu]</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="char.cpp" line="52"/>
+        <location filename="../char.cpp" line="52"/>
         <source>[Mode Menu]</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="char.cpp" line="566"/>
-        <location filename="char.cpp" line="577"/>
+        <location filename="../char.cpp" line="566"/>
+        <location filename="../char.cpp" line="577"/>
         <source>Unnamed</source>
         <comment>Character set name</comment>
         <translation type="unfinished"></translation>
