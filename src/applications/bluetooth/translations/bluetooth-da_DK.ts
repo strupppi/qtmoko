09ba74d3e03aa44bfdc8e07bbf1577848700fc78
Yann Dirson <ydirson@free.fr>
2012-03-25 12:56:25 +0200

    Normalize handling of bluetooth translations using TRANSLATION_SUBDIR.

---
+++ src/applications/bluetooth/translations/bluetooth-da_DK.ts
@@ -0,0 +1,312 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="da_DK">
+<context>
+    <name>BtFtp</name>
+    <message>
+        <location filename="../mainwindow.cpp" line="457"/>
+        <source>Browse</source>
+        <translation>Gennemse</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="459"/>
+        <source>Browse a device</source>
+        <translation>Gennemse en enhed</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="461"/>
+        <source>Delete</source>
+        <translation>Slette</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="463"/>
+        <source>Delete File or Folder</source>
+        <translation>Slette fil eller mappe</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="465"/>
+        <source>New Folder</source>
+        <translation>Ny mappe</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="467"/>
+        <source>Create Folder</source>
+        <translation>Opret mappe</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="469"/>
+        <source>Put...</source>
+        <translation>Putte...</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="471"/>
+        <source>Put File</source>
+        <translation>Putte fil</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="473"/>
+        <source>Disconnect</source>
+        <translation>Afbryd</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="475"/>
+        <source>Disconnect from Server</source>
+        <translation>Afbryd til server</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="477"/>
+        <source>Cancel</source>
+        <translation>Annullere</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="479"/>
+        <source>Cancel current action</source>
+        <translation>Annullere nuværende handling</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="528"/>
+        <source>Bluetooth FTP Client</source>
+        <translation>Bluetooth FTP klient</translation>
+    </message>
+</context>
+<context>
+    <name>BtFtpPrivate</name>
+    <message>
+        <location filename="../mainwindow.cpp" line="584"/>
+        <location filename="../mainwindow.cpp" line="887"/>
+        <location filename="../mainwindow.cpp" line="1054"/>
+        <location filename="../mainwindow.cpp" line="1106"/>
+        <location filename="../mainwindow.cpp" line="1130"/>
+        <location filename="../mainwindow.cpp" line="1168"/>
+        <source>Bluetooth error</source>
+        <translation>Bluetooth fejl</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="585"/>
+        <source>There are no Bluetooth devices present!</source>
+        <translation>Der er ingen Bluetooth enheder tilstede!</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="590"/>
+        <source>Starting...</source>
+        <translation>Starter...</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="597"/>
+        <source>Could not start Bluetooth session!</source>
+        <translation>Kunne ikke starte Bluetooth session!</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="629"/>
+        <source>Searching...</source>
+        <translation>Søger...</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="638"/>
+        <source>Canceling...</source>
+        <translation>Annullerer...</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="648"/>
+        <source>Connecting...</source>
+        <translation>Forbinder...</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="652"/>
+        <location filename="../mainwindow.cpp" line="668"/>
+        <location filename="../mainwindow.cpp" line="680"/>
+        <location filename="../mainwindow.cpp" line="823"/>
+        <source>Connection error</source>
+        <translation>Forbindelsesfejl</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="653"/>
+        <source>Unable to connect.  Please ensure the Bluetooth device is in range and try again.</source>
+        <translation>Ikke i stand til at forbinde. Venligst sikre at Bluetooth enheden er inden for område og prøv igen.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="669"/>
+        <source>The selected device does not support this service!</source>
+        <translation>Den valgte enhed understøtter ikke denne tjeneste!</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="681"/>
+        <location filename="../mainwindow.cpp" line="730"/>
+        <source>Could not connect to the remote service!</source>
+        <translation>Kunne ikke forbinde til ekstern tjeneste!</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="824"/>
+        <source>The connection has been lost.</source>
+        <translation>Forbindelsen er tabt.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="839"/>
+        <source>Connection failed.</source>
+        <translation>Forbindelse mislykkedes.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="845"/>
+        <location filename="../mainwindow.cpp" line="849"/>
+        <source>Could not change the current working folder</source>
+        <translation>Kunne ikke ændre nuværende arbejdsmappe</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="853"/>
+        <source>Parsing the folder listing failed!</source>
+        <translation>Tolkning af mappeliste mislykkedes!</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="855"/>
+        <source>Could not obtain the folder listing</source>
+        <translation>Kunne ikke indhente mappeliste</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="862"/>
+        <source>Get failed</source>
+        <translation>Hent mislykkedes</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="865"/>
+        <source>Put failed</source>
+        <translation>Putte mislykkedes</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="869"/>
+        <source>Could not create a new folder</source>
+        <translation>Kunne ikke oprette en ny mappe</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="872"/>
+        <source>Could not remove folder</source>
+        <translation>Kunne ikke fjerne mappe</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="875"/>
+        <source>Could not remove file</source>
+        <translation>Kunne ikke fjerne fil</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="992"/>
+        <source>Really Disconnect?</source>
+        <translation>Virkelig afbryde?</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="993"/>
+        <source>&lt;qt&gt;You are currently connected to a remote device, do you really wish to disconnect?&lt;/qt&gt;</source>
+        <translation>&lt;qt&gt;Du er i øjeblikket forbundet til en ekstern tjeneste, ønsker du virkelig at afbryde?&lt;/qt&gt;</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="1041"/>
+        <source>Delete</source>
+        <translation>Slette</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="1055"/>
+        <source>Removing directory failed.</source>
+        <translation>Fjerner mappe mislykkedes.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="1076"/>
+        <source>New Folder</source>
+        <translation>Ny mappe</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="1077"/>
+        <source>Enter folder name:</source>
+        <translation>Indtast mappe navn:</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="1105"/>
+        <location filename="../mainwindow.cpp" line="1167"/>
+        <source>Document System Error</source>
+        <translation>Dokument system fejl</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="1129"/>
+        <source>Not enough free disk space for this file.</source>
+        <translation>Ikke nok fri lagerplads til denne fil.</translation>
+    </message>
+</context>
+<context>
+    <name>DirDeleterDialog</name>
+    <message>
+        <location filename="../dirdeleterdialog.cpp" line="187"/>
+        <source>Removing Folder: </source>
+        <translation>Fjerner mappe:</translation>
+    </message>
+    <message>
+        <location filename="../dirdeleterdialog.cpp" line="200"/>
+        <source>Cancel</source>
+        <translation>Annullere</translation>
+    </message>
+</context>
+<context>
+    <name>ObexFtpModel</name>
+    <message>
+        <location filename="../mainwindow.cpp" line="156"/>
+        <source>Parent Folder</source>
+        <translation>Overmappe</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="215"/>
+        <source>Name</source>
+        <translation>Navn</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="217"/>
+        <source>Size</source>
+        <translation>Størrelse</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="219"/>
+        <source>Last Modified</source>
+        <translation>Sidst ændret</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="221"/>
+        <source>Created On</source>
+        <translation>Oprettet den</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="223"/>
+        <source>Last Accessed</source>
+        <translation>Sidst tilgået</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="225"/>
+        <source>User Permissions</source>
+        <translation>Bruger tilladelser</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="227"/>
+        <source>Group Permissions</source>
+        <translation>Gruppe tilladelser</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="229"/>
+        <source>Other Permissions</source>
+        <translation>Andre tilladelser</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="231"/>
+        <source>Owner</source>
+        <translation>Ejer</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="233"/>
+        <source>Group</source>
+        <translation>Gruppe</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="235"/>
+        <source>Mimetype</source>
+        <translation>MIME-type</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="237"/>
+        <source>Description</source>
+        <translation>Beskrivelse</translation>
+    </message>
+</context>
+</TS>
