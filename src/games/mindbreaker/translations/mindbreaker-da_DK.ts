21bf7676605aabfe977801ca808cfb45d1b59f7f
Yann Dirson <ydirson@free.fr>
2012-03-25 14:09:44 +0200

    Normalize handling of games/mindbreaker translations using TRANSLATION_SUBDIR.

---
+++ src/games/mindbreaker/translations/mindbreaker-da_DK.ts
@@ -0,0 +1,60 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="da_DK">
+<context>
+    <name>MindBreaker</name>
+    <message>
+        <location filename="../mindbreaker.cpp" line="273"/>
+        <source>Mindbreaker</source>
+        <translation>Hukommelse</translation>
+    </message>
+    <message>
+        <location filename="../mindbreaker.cpp" line="288"/>
+        <source>New Game</source>
+        <translation>Nyt spil</translation>
+    </message>
+    <message>
+        <location filename="../mindbreaker.cpp" line="289"/>
+        <source>Start a new game</source>
+        <translation>Start et nyt spil</translation>
+    </message>
+    <message>
+        <location filename="../mindbreaker.cpp" line="291"/>
+        <source>Reset Score</source>
+        <translation>Nulstil score</translation>
+    </message>
+    <message>
+        <location filename="../mindbreaker.cpp" line="292"/>
+        <source>Reset scores</source>
+        <translation>Nulstil scorerne</translation>
+    </message>
+    <message>
+        <location filename="../mindbreaker.cpp" line="322"/>
+        <source>win avg: %1 turns (%2 games)</source>
+        <translation>vinder gns.: %1 omgange (%2 spil)</translation>
+    </message>
+</context>
+<context>
+    <name>MindBreakerBoard</name>
+    <message>
+        <location filename="../mindbreaker.cpp" line="686"/>
+        <source>Reset Statistics</source>
+        <translation>Nulstil statistikker</translation>
+    </message>
+    <message>
+        <location filename="../mindbreaker.cpp" line="687"/>
+        <source>&lt;qt&gt;Reset the win ratio?&lt;/qt&gt;</source>
+        <translation>&lt;qt&gt;Nulstil vinder forhold?&lt;/qt&gt;</translation>
+    </message>
+    <message>
+        <location filename="../mindbreaker.cpp" line="688"/>
+        <source>OK</source>
+        <translation>O.k.</translation>
+    </message>
+    <message>
+        <location filename="../mindbreaker.cpp" line="688"/>
+        <source>Cancel</source>
+        <translation>Annullere</translation>
+    </message>
+</context>
+</TS>
--- src/games/mindbreaker/translations/mindbreaker-it_IT.ts
+++ src/games/mindbreaker/translations/mindbreaker-it_IT.ts
@@ -4,32 +4,32 @@
 <context>
     <name>MindBreaker</name>
     <message>
-        <location filename="mindbreaker.cpp" line="273"/>
+        <location filename="../mindbreaker.cpp" line="273"/>
         <source>Mindbreaker</source>
         <translation>Rompicapo</translation>
     </message>
     <message>
-        <location filename="mindbreaker.cpp" line="288"/>
+        <location filename="../mindbreaker.cpp" line="288"/>
         <source>New Game</source>
         <translation>Nuovo rompicapo</translation>
     </message>
     <message>
-        <location filename="mindbreaker.cpp" line="289"/>
+        <location filename="../mindbreaker.cpp" line="289"/>
         <source>Start a new game</source>
         <translation>Inizia un nuovo gioco</translation>
     </message>
     <message>
-        <location filename="mindbreaker.cpp" line="291"/>
+        <location filename="../mindbreaker.cpp" line="291"/>
         <source>Reset Score</source>
         <translation>Resetta i punteggi</translation>
     </message>
     <message>
-        <location filename="mindbreaker.cpp" line="292"/>
+        <location filename="../mindbreaker.cpp" line="292"/>
         <source>Reset scores</source>
         <translation>Resetta i punteggi</translation>
     </message>
     <message>
-        <location filename="mindbreaker.cpp" line="322"/>
+        <location filename="../mindbreaker.cpp" line="322"/>
         <source>win avg: %1 turns (%2 games)</source>
         <translation>Media soluti: %1 turni (su %2 partite)</translation>
     </message>
@@ -37,22 +37,22 @@
 <context>
     <name>MindBreakerBoard</name>
     <message>
-        <location filename="mindbreaker.cpp" line="686"/>
+        <location filename="../mindbreaker.cpp" line="686"/>
         <source>Reset Statistics</source>
         <translation>Resetta statistiche</translation>
     </message>
     <message>
-        <location filename="mindbreaker.cpp" line="687"/>
+        <location filename="../mindbreaker.cpp" line="687"/>
         <source>&lt;qt&gt;Reset the win ratio?&lt;/qt&gt;</source>
         <translation>&lt;qt&gt;Vuoi davvero cancellare i dati fin quì registrati?&lt;/qt&gt;</translation>
     </message>
     <message>
-        <location filename="mindbreaker.cpp" line="688"/>
+        <location filename="../mindbreaker.cpp" line="688"/>
         <source>OK</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="mindbreaker.cpp" line="688"/>
+        <location filename="../mindbreaker.cpp" line="688"/>
         <source>Cancel</source>
         <translation>Annulla</translation>
     </message>
