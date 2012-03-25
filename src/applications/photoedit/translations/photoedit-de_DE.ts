12740758fddabc6984c0abfd995e6afbc7f5ff70
Yann Dirson <ydirson@free.fr>
2012-03-25 13:14:34 +0200

    Normalize handling of photoedit translations using TRANSLATION_SUBDIR.

--- src/applications/photoedit/translations/photoedit-de_DE.ts
+++ src/applications/photoedit/translations/photoedit-de_DE.ts
@@ -4,8 +4,8 @@
 <context>
     <name>EffectDialog</name>
     <message>
-        <location filename="effectdialog.cpp" line="58"/>
-        <location filename="effectdialog.cpp" line="77"/>
+        <location filename="../effectdialog.cpp" line="58"/>
+        <location filename="../effectdialog.cpp" line="77"/>
         <source>Effects</source>
         <translation type="unfinished"></translation>
     </message>
@@ -13,7 +13,7 @@
 <context>
     <name>EffectModel</name>
     <message>
-        <location filename="effectmodel.cpp" line="150"/>
+        <location filename="../effectmodel.cpp" line="150"/>
         <source>None</source>
         <translation type="unfinished"></translation>
     </message>
@@ -21,277 +21,277 @@
 <context>
     <name>PhotoEditUI</name>
     <message>
-        <location filename="photoeditui.cpp" line="113"/>
-        <location filename="photoeditui.cpp" line="864"/>
+        <location filename="../photoeditui.cpp" line="113"/>
+        <location filename="../photoeditui.cpp" line="864"/>
         <source>Pictures</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="photoeditui.cpp" line="171"/>
-        <location filename="photoeditui.cpp" line="199"/>
+        <location filename="../photoeditui.cpp" line="171"/>
+        <location filename="../photoeditui.cpp" line="199"/>
         <source>Back</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="photoeditui.cpp" line="229"/>
+        <location filename="../photoeditui.cpp" line="229"/>
         <source>Actual Size</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="photoeditui.cpp" line="230"/>
+        <location filename="../photoeditui.cpp" line="230"/>
         <source>Best Fit</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="photoeditui.cpp" line="252"/>
-        <location filename="photoeditui.cpp" line="390"/>
+        <location filename="../photoeditui.cpp" line="252"/>
+        <location filename="../photoeditui.cpp" line="390"/>
         <source>Slide Show...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="photoeditui.cpp" line="259"/>
-        <location filename="photoeditui.cpp" line="394"/>
+        <location filename="../photoeditui.cpp" line="259"/>
+        <location filename="../photoeditui.cpp" line="394"/>
         <source>Edit</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="photoeditui.cpp" line="262"/>
-        <location filename="photoeditui.cpp" line="397"/>
+        <location filename="../photoeditui.cpp" line="262"/>
+        <location filename="../photoeditui.cpp" line="397"/>
         <source>Properties</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="photoeditui.cpp" line="263"/>
-        <location filename="photoeditui.cpp" line="399"/>
+        <location filename="../photoeditui.cpp" line="263"/>
+        <location filename="../photoeditui.cpp" line="399"/>
         <source>Send</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="photoeditui.cpp" line="265"/>
-        <location filename="photoeditui.cpp" line="402"/>
+        <location filename="../photoeditui.cpp" line="265"/>
+        <location filename="../photoeditui.cpp" line="402"/>
         <source>Print</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="photoeditui.cpp" line="266"/>
-        <location filename="photoeditui.cpp" line="405"/>
-        <location filename="photoeditui.cpp" line="1371"/>
+        <location filename="../photoeditui.cpp" line="266"/>
+        <location filename="../photoeditui.cpp" line="405"/>
+        <location filename="../photoeditui.cpp" line="1371"/>
         <source>Delete</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="photoeditui.cpp" line="268"/>
-        <location filename="photoeditui.cpp" line="407"/>
+        <location filename="../photoeditui.cpp" line="268"/>
+        <location filename="../photoeditui.cpp" line="407"/>
         <source>Set as Background</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="photoeditui.cpp" line="270"/>
-        <location filename="photoeditui.cpp" line="411"/>
+        <location filename="../photoeditui.cpp" line="270"/>
+        <location filename="../photoeditui.cpp" line="411"/>
         <source>Save to Contact...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="photoeditui.cpp" line="271"/>
-        <location filename="photoeditui.cpp" line="413"/>
+        <location filename="../photoeditui.cpp" line="271"/>
+        <location filename="../photoeditui.cpp" line="413"/>
         <source>Set as Avatar</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="photoeditui.cpp" line="277"/>
+        <location filename="../photoeditui.cpp" line="277"/>
         <source>Effects...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="photoeditui.cpp" line="283"/>
-        <location filename="photoeditui.cpp" line="544"/>
+        <location filename="../photoeditui.cpp" line="283"/>
+        <location filename="../photoeditui.cpp" line="544"/>
         <source>Full Screen</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="photoeditui.cpp" line="287"/>
+        <location filename="../photoeditui.cpp" line="287"/>
         <source>Zoom...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="photoeditui.cpp" line="293"/>
-        <location filename="photoeditui.cpp" line="531"/>
-        <location filename="photoeditui.cpp" line="541"/>
+        <location filename="../photoeditui.cpp" line="293"/>
+        <location filename="../photoeditui.cpp" line="531"/>
+        <location filename="../photoeditui.cpp" line="541"/>
         <source>Zoom</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="photoeditui.cpp" line="328"/>
-        <location filename="photoeditui.cpp" line="330"/>
-        <location filename="photoeditui.cpp" line="699"/>
+        <location filename="../photoeditui.cpp" line="328"/>
+        <location filename="../photoeditui.cpp" line="330"/>
+        <location filename="../photoeditui.cpp" line="699"/>
         <source>Type: %1</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="photoeditui.cpp" line="330"/>
-        <location filename="photoeditui.cpp" line="347"/>
+        <location filename="../photoeditui.cpp" line="330"/>
+        <location filename="../photoeditui.cpp" line="347"/>
         <source>(Multi)</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="photoeditui.cpp" line="337"/>
-        <location filename="photoeditui.cpp" line="343"/>
-        <location filename="photoeditui.cpp" line="347"/>
-        <location filename="photoeditui.cpp" line="747"/>
+        <location filename="../photoeditui.cpp" line="337"/>
+        <location filename="../photoeditui.cpp" line="343"/>
+        <location filename="../photoeditui.cpp" line="347"/>
+        <location filename="../photoeditui.cpp" line="747"/>
         <source>Category: %1</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="photoeditui.cpp" line="337"/>
+        <location filename="../photoeditui.cpp" line="337"/>
         <source>Unfiled</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="photoeditui.cpp" line="419"/>
+        <location filename="../photoeditui.cpp" line="419"/>
         <source>View Type...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="photoeditui.cpp" line="420"/>
+        <location filename="../photoeditui.cpp" line="420"/>
         <source>View Category...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="photoeditui.cpp" line="537"/>
+        <location filename="../photoeditui.cpp" line="537"/>
         <source>Crop</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="photoeditui.cpp" line="538"/>
+        <location filename="../photoeditui.cpp" line="538"/>
         <source>Brightness</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="photoeditui.cpp" line="539"/>
+        <location filename="../photoeditui.cpp" line="539"/>
         <source>Rotate</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="photoeditui.cpp" line="549"/>
+        <location filename="../photoeditui.cpp" line="549"/>
         <source>Save As...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="photoeditui.cpp" line="550"/>
+        <location filename="../photoeditui.cpp" line="550"/>
         <source>Cancel</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="photoeditui.cpp" line="677"/>
+        <location filename="../photoeditui.cpp" line="677"/>
         <source>View Type</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="photoeditui.cpp" line="725"/>
+        <location filename="../photoeditui.cpp" line="725"/>
         <source>View Category</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="photoeditui.cpp" line="959"/>
+        <location filename="../photoeditui.cpp" line="959"/>
         <source>Scaled Image</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="photoeditui.cpp" line="960"/>
+        <location filename="../photoeditui.cpp" line="960"/>
         <source>&lt;qt&gt;Unable to load complete image.  A scaled copy has been opened instead.&lt;/qt&gt;</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="photoeditui.cpp" line="1010"/>
+        <location filename="../photoeditui.cpp" line="1010"/>
         <source>Load Error</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="photoeditui.cpp" line="1011"/>
+        <location filename="../photoeditui.cpp" line="1011"/>
         <source>&lt;qt&gt;Unable to load image.&lt;/qt&gt;</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="photoeditui.cpp" line="1019"/>
-        <location filename="photoeditui.cpp" line="1034"/>
+        <location filename="../photoeditui.cpp" line="1019"/>
+        <location filename="../photoeditui.cpp" line="1034"/>
         <source>Size Error</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="photoeditui.cpp" line="1020"/>
+        <location filename="../photoeditui.cpp" line="1020"/>
         <source>&lt;qt&gt;Unable to edit image of size %1x%2.  The maximum editable size is %3x%4&lt;/qt&gt;</source>
         <comment>%1x%2 = image width and height, %3x%4 = maximum width and height</comment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="photoeditui.cpp" line="1035"/>
+        <location filename="../photoeditui.cpp" line="1035"/>
         <source>&lt;qt&gt;Unable to edit image.  The maximum editable size is %1x%2&lt;/qt&gt;</source>
         <comment>%1x%2 = maximum width and height</comment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="photoeditui.cpp" line="1059"/>
+        <location filename="../photoeditui.cpp" line="1059"/>
         <source>Depth Error</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="photoeditui.cpp" line="1060"/>
+        <location filename="../photoeditui.cpp" line="1060"/>
         <source>&lt;qt&gt;Image depth is not supported.&lt;/qt&gt;</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="photoeditui.cpp" line="1372"/>
+        <location filename="../photoeditui.cpp" line="1372"/>
         <source>&lt;qt&gt;Are you sure you want to delete %1?&lt;/qt&gt;</source>
         <comment> %1 = file name</comment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="photoeditui.cpp" line="1442"/>
+        <location filename="../photoeditui.cpp" line="1442"/>
         <source>Background Image</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="photoeditui.cpp" line="1443"/>
+        <location filename="../photoeditui.cpp" line="1443"/>
         <source>Could not save image to permanent storage</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="photoeditui.cpp" line="1678"/>
+        <location filename="../photoeditui.cpp" line="1678"/>
         <source>Save Changes </source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="photoeditui.cpp" line="1679"/>
+        <location filename="../photoeditui.cpp" line="1679"/>
         <source>&lt;qt&gt;Do you want to save your changes?&lt;/qt&gt;</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="photoeditui.cpp" line="1687"/>
+        <location filename="../photoeditui.cpp" line="1687"/>
         <source>Read-Only File</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="photoeditui.cpp" line="1688"/>
+        <location filename="../photoeditui.cpp" line="1688"/>
         <source>&lt;qt&gt;Saving a copy of the read-only file.&lt;/qt&gt;</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="photoeditui.cpp" line="1699"/>
+        <location filename="../photoeditui.cpp" line="1699"/>
         <source>Saving %1</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="photoeditui.cpp" line="1700"/>
+        <location filename="../photoeditui.cpp" line="1700"/>
         <source>&lt;qt&gt;Saving as %1 is not supported. Using the default format instead.&lt;/qt&gt;</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="photoeditui.cpp" line="1723"/>
+        <location filename="../photoeditui.cpp" line="1723"/>
         <source>Save failed</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="photoeditui.cpp" line="1724"/>
+        <location filename="../photoeditui.cpp" line="1724"/>
         <source>&lt;qt&gt;Your edits were not saved.&lt;/qt&gt;</source>
         <translation type="unfinished"></translation>
     </message>
@@ -299,12 +299,12 @@
 <context>
     <name>RegionSelector</name>
     <message>
-        <location filename="editor/regionselector.cpp" line="330"/>
+        <location filename="../editor/regionselector.cpp" line="330"/>
         <source>Size</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="editor/regionselector.cpp" line="333"/>
+        <location filename="../editor/regionselector.cpp" line="333"/>
         <source>Move</source>
         <translation type="unfinished"></translation>
     </message>
@@ -312,22 +312,22 @@
 <context>
     <name>SlideShowDialog</name>
     <message>
-        <location filename="slideshow/slideshowdialog.cpp" line="32"/>
+        <location filename="../slideshow/slideshowdialog.cpp" line="32"/>
         <source>Slide Show</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="slideshow/slideshowdialog.cpp" line="50"/>
+        <location filename="../slideshow/slideshowdialog.cpp" line="50"/>
         <source>Display names</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="slideshow/slideshowdialog.cpp" line="54"/>
+        <location filename="../slideshow/slideshowdialog.cpp" line="54"/>
         <source>Loop through</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="slideshow/slideshowdialog.h" line="46"/>
+        <location filename="../slideshow/slideshowdialog.h" line="46"/>
         <source>Slide length ( %1 sec. )</source>
         <translation type="unfinished"></translation>
     </message>
