3d0ca4ed28f0ac930b05590c74e85cacddc43119
Yann Dirson <ydirson@free.fr>
2012-03-25 16:55:36 +0200

    Normalize handling of libraries/qtopiaprinting translations using TRANSLATION_SUBDIR.

--- src/libraries/qtopiaprinting/translations/libqtopiaprinting-cs_CZ.ts
+++ src/libraries/qtopiaprinting/translations/libqtopiaprinting-cs_CZ.ts
@@ -1,104 +1,105 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="1.1" language="cs_CZ">
+<!DOCTYPE TS>
+<TS version="2.0" language="cs_CZ">
 <context>
     <name>PrintDialogBase</name>
     <message>
-        <location filename="printdialogbase.ui" line="16"/>
+        <location filename="../printdialogbase.ui" line="16"/>
         <source>Print Dialog</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="printdialogbase.ui" line="32"/>
+        <location filename="../printdialogbase.ui" line="32"/>
         <source>Destination</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="printdialogbase.ui" line="44"/>
+        <location filename="../printdialogbase.ui" line="44"/>
         <source>Print destination</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="printdialogbase.ui" line="56"/>
+        <location filename="../printdialogbase.ui" line="56"/>
         <source>Print to printer</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="printdialogbase.ui" line="66"/>
+        <location filename="../printdialogbase.ui" line="66"/>
         <source>Print to file</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="printdialogbase.ui" line="97"/>
+        <location filename="../printdialogbase.ui" line="97"/>
         <source>Printer</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="printdialogbase.ui" line="109"/>
+        <location filename="../printdialogbase.ui" line="109"/>
         <source>Printer settings</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="printdialogbase.ui" line="121"/>
+        <location filename="../printdialogbase.ui" line="121"/>
         <source>Print in color if available</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="printdialogbase.ui" line="131"/>
+        <location filename="../printdialogbase.ui" line="131"/>
         <source>Print in grayscale</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="printdialogbase.ui" line="141"/>
+        <location filename="../printdialogbase.ui" line="141"/>
         <source>Paper format</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="printdialogbase.ui" line="154"/>
+        <location filename="../printdialogbase.ui" line="154"/>
         <source>Portrait</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="printdialogbase.ui" line="159"/>
+        <location filename="../printdialogbase.ui" line="159"/>
         <source>Landscape</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="printdialogbase.ui" line="187"/>
+        <location filename="../printdialogbase.ui" line="187"/>
         <source>Options</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="printdialogbase.ui" line="207"/>
+        <location filename="../printdialogbase.ui" line="207"/>
         <source>No. of copies</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="printdialogbase.ui" line="223"/>
+        <location filename="../printdialogbase.ui" line="223"/>
         <source>Print range</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="printdialogbase.ui" line="254"/>
+        <location filename="../printdialogbase.ui" line="254"/>
         <source>From</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="printdialogbase.ui" line="281"/>
+        <location filename="../printdialogbase.ui" line="281"/>
         <source>To</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="printdialogbase.ui" line="305"/>
+        <location filename="../printdialogbase.ui" line="305"/>
         <source>Print order</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="printdialogbase.ui" line="318"/>
+        <location filename="../printdialogbase.ui" line="318"/>
         <source>Print first page first</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="printdialogbase.ui" line="323"/>
+        <location filename="../printdialogbase.ui" line="323"/>
         <source>Print last page last</source>
         <translation type="unfinished"></translation>
     </message>
