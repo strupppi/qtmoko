0bb6e45b1bc2c890d13de41a067dedb2e0719c19
Stefan Rupp <struppi@chaostreff.in>
2012-08-17 22:38:01 +0200

    first steps towards running as unprivileged user

--- devices/gta04/src/devtools/startup/qpe.sh
+++ devices/gta04/src/devtools/startup/qpe.sh
@@ -3,9 +3,9 @@
 
 if [ ! -d /dev/pts ]; then
     # mount pts so that terminal and ssh work (only needed when using devtmpfs)
-    mkdir -p /dev/pts
-    mount -t devpts none /dev/pts
-    modprobe joydev
+    sudo mkdir -p /dev/pts
+    sudo mount -t devpts none /dev/pts
+    sudo modprobe joydev
 fi
 
 if [ ! "$(pidof udevd)" ]; then
@@ -14,25 +14,25 @@ if [ ! "$(pidof udevd)" ]; then
 fi
 
 # Saves power in suspend
-rfkill block bluetooth &
+sudo rfkill block bluetooth &
 
 # Power on modem on GTA04A4 and higher
 modem-poweron.sh
 
-rm -f /dev/input/mice
-echo '1' > /proc/sys/kernel/printk
+sudo rm -f /dev/input/mice
+echo '1' | sudo tee /proc/sys/kernel/printk
 echo 0 > /sys/devices/virtual/graphics/fbcon/cursor_blink
 echo timer > /sys/class/leds/gta04:green:power/trigger
 echo 0 > /sys/class/leds/gta04\:red\:power/brightness
 echo 0 > /sys/class/leds/gta04\:green\:power/brightness
 stty -F /dev/tty1 -echo
-mkdir -p /var/cache/apt/archives/partial
+sudo mkdir -p /var/cache/apt/archives/partial
 
-atd /var/spool/at
+sudo atd /var/spool/at
 
 touch /tmp/restart-qtopia
 while [ -e /tmp/restart-qtopia ]; do
     qpe
 done
 
-killall -q atd
+sudo killall -q atd
