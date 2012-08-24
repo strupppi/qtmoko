a492100df0c2c663f0d40ff83a496163da9e2556
Stefan Rupp <struppi@chaostreff.in>
2012-08-24 05:30:02 +0200

    qtmoko now starts up the GUI as user "qtmoko"

--- devices/gta04/src/devtools/startup/qpe.sh
+++ devices/gta04/src/devtools/startup/qpe.sh
@@ -3,9 +3,9 @@
 
 if [ ! -d /dev/pts ]; then
     # mount pts so that terminal and ssh work (only needed when using devtmpfs)
-    sudo mkdir -p /dev/pts
-    sudo mount -t devpts none /dev/pts
-    sudo modprobe joydev
+    mkdir -p /dev/pts
+    mount -t devpts none /dev/pts
+    modprobe joydev
 fi
 
 if [ ! "$(pidof udevd)" ]; then
@@ -14,25 +14,25 @@ if [ ! "$(pidof udevd)" ]; then
 fi
 
 # Saves power in suspend
-sudo rfkill block bluetooth &
+rfkill block bluetooth &
 
 # Power on modem on GTA04A4 and higher
 modem-poweron.sh
 
-sudo rm -f /dev/input/mice
-echo '1' | sudo tee /proc/sys/kernel/printk
+rm -f /dev/input/mice
+echo '1' > /proc/sys/kernel/printk
 echo 0 > /sys/devices/virtual/graphics/fbcon/cursor_blink
 echo timer > /sys/class/leds/gta04:green:power/trigger
 echo 0 > /sys/class/leds/gta04\:red\:power/brightness
 echo 0 > /sys/class/leds/gta04\:green\:power/brightness
 stty -F /dev/tty1 -echo
-sudo mkdir -p /var/cache/apt/archives/partial
+mkdir -p /var/cache/apt/archives/partial
 
-sudo atd /var/spool/at
+atd /var/spool/at
 
 touch /tmp/restart-qtopia
 while [ -e /tmp/restart-qtopia ]; do
-    qpe
+    su $USER -l -c ". /opt/qtmoko/qpe.env; qpe"
 done
 
-sudo killall -q atd
+killall -q atd
